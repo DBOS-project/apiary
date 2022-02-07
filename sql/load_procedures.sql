38a3d78273e9d6724ea3f87a13b1407a8a2ccae4
Qian Li <liqian.cs@gmail.com>
2022-02-07 20:44:54 +0000

    compile with maven

--- sql/load_procedures.sql
+++ sql/load_procedures.sql
@@ -1,4 +1,4 @@
-load classes target/ApiaryProcedures.jar;
+load classes target/dynamic-apiary-0.1-SNAPSHOT.jar;
 
 DROP PROCEDURE IncrementVSP IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE IncrementTable COLUMN PKey PARAMETER 0 FROM CLASS org.dbos.apiary.procedures.IncrementVSP;

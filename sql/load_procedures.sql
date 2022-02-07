ab408e857d0088689765b07077691f96f97f4689
Qian Li <liqian.cs@gmail.com>
2022-02-07 14:06:28 -0800

    Compile (#2)
    * compile with maven

--- sql/load_procedures.sql
+++ sql/load_procedures.sql
@@ -1,4 +1,7 @@
-load classes target/ApiaryProcedures.jar;
+load classes target/dynamic-apiary-0.1-SNAPSHOT.jar;
 
 DROP PROCEDURE IncrementVSP IF EXISTS;
 CREATE PROCEDURE PARTITION ON TABLE IncrementTable COLUMN PKey PARAMETER 0 FROM CLASS org.dbos.apiary.procedures.IncrementVSP;
+
+DROP PROCEDURE TruncateTables IF EXISTS;
+CREATE PROCEDURE FROM CLASS org.dbos.apiary.procedures.TruncateTables;
--- src/test/java/org/dbos/apiary/HelloWorldTest.java
+++ src/test/java/org/dbos/apiary/HelloWorldTest.java
@@ -2,6 +2,7 @@ package org.dbos.apiary;
 
 import org.dbos.apiary.context.ApiaryContext;
 import org.dbos.apiary.utilities.Utilities;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
     private static final Logger logger = LoggerFactory.getLogger(HelloWorldTest.class);
+    @BeforeEach
+    public void truncateTables() throws IOException, ProcCallException {
+        ApiaryContext ctxt = new ApiaryContext("localhost", 21212);
+        ctxt.client.callProcedure("TruncateTables");
+    }
 
     @Test
     public void testHelloWorld() {
@@ -30,6 +36,10 @@ public class HelloWorldTest {
         VoltTable[] res = ctxt.client.callProcedure("IncrementVSP", 0, 1L).getResults();
         assertEquals(1, res.length);
         long resVal = res[0].fetchRow(0).getLong(0);
-        assertEquals(1, resVal);
+        assertEquals(1L, resVal);
+        // Run again, increment by one.
+        res = ctxt.client.callProcedure("IncrementVSP", 0, 1L).getResults();
+        resVal = res[0].fetchRow(0).getLong(0);
+        assertEquals(2L, resVal);
     }
 }

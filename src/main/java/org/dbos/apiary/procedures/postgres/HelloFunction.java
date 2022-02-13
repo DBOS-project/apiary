92375fbd7d133d2f8a46a72b00326c928884703c
Qian Li <liqian.cs@gmail.com>
2022-02-13 01:00:43 -0800

    update

--- src/main/java/org/dbos/apiary/procedures/postgres/HelloFunction.java
+++ src/main/java/org/dbos/apiary/procedures/postgres/HelloFunction.java
@@ -2,9 +2,23 @@ package org.dbos.apiary.procedures.postgres;
 
 import org.postgresql.pljava.annotation.Function;
 
+import java.sql.*;
+
 public class HelloFunction {
+    private static String m_url = "jdbc:default:connection";
+
     @Function
-    public static String greet(String personName) {
-        return "Hello World, " + personName + " !";
+    public static String greet(String personName) throws SQLException {
+        Connection conn = DriverManager.getConnection( m_url );
+        String query = "SELECT * FROM KVTable WHERE KVKey = ?";
+        PreparedStatement stmt = conn.prepareStatement( query );
+        stmt.setInt(1, 0);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        int val = rs.getInt("KVvalue");
+        stmt.close();
+        conn.close();
+
+        return "Hello World, " + personName + " " + val + " !";
     }
 }

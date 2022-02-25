a525a34e22a86579ddc35af37cfdb274d038fc22
Qian Li <liqian.cs@gmail.com>
2022-02-25 05:54:39 +0000

    fix update procedure script

--- scripts/update_voltdb_procedures.sh
+++ scripts/update_voltdb_procedures.sh
@@ -10,5 +10,5 @@ VOLTDB_BIN="${VOLT_HOME}/bin"
 mvn -DskipTests package
 
 javac -cp "$VOLT_HOME/voltdb/*:$PWD/target/*" -d obj/sql $(find src/main/java/org/dbos/apiary/procedures/voltdb -type f -name *.java)
-jar cvf target/DBOSProcedures.jar -C obj/sql . -C target/classes org/dbos/apiary/interposition -C target/classes org/dbos/apiary/voltdb -C target/classes org/dbos/apiary/executor -C target/classes org/dbos/apiary/utilities -C target/classes org/dbos/apiary/stateless -C target/classes org/dbos/apiary/procedures/stateless -C target/classes org/dbos/apiary/introspect
+jar cvf target/DBOSProcedures.jar -C obj/sql . -C target/classes org/dbos/apiary/interposition -C target/classes org/dbos/apiary/voltdb -C target/classes org/dbos/apiary/executor -C target/classes org/dbos/apiary/utilities -C target/classes org/dbos/apiary/stateless -C target/classes org/dbos/apiary/procedures/stateless
 ${VOLTDB_BIN}/sqlcmd < sql/load_procedures.sql

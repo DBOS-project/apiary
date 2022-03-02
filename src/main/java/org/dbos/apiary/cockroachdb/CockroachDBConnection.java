1f8d2d4d1fd25277c6cc7a0851bdb33019b9b43d
Danny Cho <dannycho7@gmail.com>
2022-03-01 21:51:41 -0800

    Working w/o locality

---
+++ src/main/java/org/dbos/apiary/cockroachdb/CockroachDBConnection.java
@@ -0,0 +1,60 @@
+package org.dbos.apiary.cockroachdb;
+
+import org.dbos.apiary.executor.ApiaryConnection;
+import org.dbos.apiary.executor.FunctionOutput;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class CockroachDBConnection implements ApiaryConnection {
+
+    private final Connection c;
+    private final Map<String, Callable<CockroachDBFunctionInterface>> functions = new HashMap<>();
+
+    public CockroachDBConnection(Connection c) throws SQLException {
+        this.c = c;
+        c.setAutoCommit(false);
+    }
+
+    public void registerFunction(String name, Callable<CockroachDBFunctionInterface> function) {
+        functions.put(name, function);
+    }
+
+    @Override
+    public FunctionOutput callFunction(String name, Object... inputs) throws Exception {
+        CockroachDBFunctionInterface function = functions.get(name).call();
+        FunctionOutput f = null;
+        try {
+            f = function.runFunction(inputs);
+            c.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            c.rollback();
+        }
+        return f;
+    }
+
+    @Override
+    public void updatePartitionInfo() {
+        return;
+    }
+
+    @Override
+    public int getNumPartitions() {
+        return 1;
+    }
+
+    @Override
+    public String getHostname(Object[] input) {
+        return "localhost";
+    }
+
+    @Override
+    public Map<Integer, String> getPartitionHostMap() {
+        return null;
+    }
+
+}
---
+++ src/main/java/org/dbos/apiary/procedures/cockroachdb/CockroachDBFibonacciFunction.java
@@ -0,0 +1,46 @@
+package org.dbos.apiary.procedures.cockroachdb;
+
+import org.dbos.apiary.interposition.ApiaryFuture;
+import org.dbos.apiary.cockroachdb.CockroachDBFunctionInterface;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CockroachDBFibonacciFunction extends CockroachDBFunctionInterface {
+
+    private final PreparedStatement addResult;
+    private final PreparedStatement getValue;
+
+    public CockroachDBFibonacciFunction(Connection c) throws SQLException {
+        this.addResult = c.prepareStatement("INSERT INTO KVTable(KVKey, KVValue) VALUES (?, ?);");
+        this.getValue = c.prepareStatement("SELECT KVValue FROM KVTable WHERE KVKey=?;");
+    }
+
+    public Object runFunction(String strKey) throws SQLException {
+        int key = Integer.parseInt(strKey);
+        if (key < 0) {
+            return "";
+        }
+        if (key == 0) {
+            this.apiaryExecuteUpdate(addResult, key, 0);
+            return "0";
+        }
+        if (key == 1) {
+            this.apiaryExecuteUpdate(addResult, key, 1);
+            return "1";
+        }
+        // Check if the number has been calculated before.
+        ResultSet r = (ResultSet) this.apiaryExecuteQuery(getValue, key);
+        if (r.next()) {
+            return String.valueOf(r.getLong(1));
+        }
+
+        // Otherwise, call functions.
+        ApiaryFuture f1 = this.apiaryQueueFunction("FibonacciFunction", String.valueOf(key - 2));
+        ApiaryFuture f2 = this.apiaryQueueFunction("FibonacciFunction", String.valueOf(key - 1));
+        ApiaryFuture fsum = this.apiaryQueueFunction("FibSumFunction", strKey, f1, f2);
+        return fsum;
+    }
+}

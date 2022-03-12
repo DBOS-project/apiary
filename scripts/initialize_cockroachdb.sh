793272e2f58c4bdffbe0e44ddbf62911883c06b8
Qian Li <liqian.cs@gmail.com>
2022-03-12 12:34:10 -0800

    Script to start a local cockroachDB (#26)
    * scripts to start a local cockroachDB
    
    * Use realpath instead of readlink
    
    Having issues with readlink on mac.
    
    * Initialize multi-node local cluster
    
    * Initialize cluster with init sql
    
    * mkdir -p for cockroach-data
    
    Co-authored-by: Danny Cho <dannycho7@gmail.com>

---
+++ scripts/initialize_cockroachdb.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+
+if [[ -z $(command -v cockroach) ]]; then
+    echo "CockroachDB not installed!"
+    # Install CockroachDB
+    cd ${HOME}
+    curl https://binaries.cockroachdb.com/cockroach-v21.2.5.linux-amd64.tgz | tar -xz
+    sudo cp -i cockroach-v21.2.5.linux-amd64/cockroach /usr/local/bin/
+    sudo mkdir -p /usr/local/lib/cockroach
+    sudo cp -i cockroach-v21.2.5.linux-amd64/lib/libgeos.so /usr/local/lib/cockroach/
+    sudo cp -i cockroach-v21.2.5.linux-amd64/lib/libgeos_c.so /usr/local/lib/cockroach/
+
+    # Only initialize once.
+    # Enter the root dir of the repo.
+    cd ${SCRIPT_DIR}/../
+
+    cockroach init --insecure --host=localhost
+fi
+
+# Enter the root dir of the repo.
+cd ${SCRIPT_DIR}/../
+
+DB_PORTS=(26257 26258 26259)
+JOIN_STR=""
+for port in ${DB_PORTS[@]}
+do
+    JOIN_STR+=,localhost:$port
+done
+JOIN_STR=${JOIN_STR:1}
+
+mkdir -p cockroach-data/
+for i in "${!DB_PORTS[@]}"; do
+    cockroach start \
+        --insecure --store=cockroach-data/node$i\
+        --listen-addr=localhost:${DB_PORTS[i]}\
+        --http-addr=localhost:$((8080 + $i))\
+        --join=$JOIN_STR\
+        --background
+done
+
+cockroach init --insecure --host=localhost:26257
+
+# Create DB and tables.
+cat sql/cockroachdb_init.sql | cockroach sql --url "postgresql://root@localhost:26257?sslmode=disable"
+
+echo "==== Finished initializing CockroachDB. ===="

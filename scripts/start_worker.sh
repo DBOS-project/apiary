8acd14eb37a7591c14fbe2907a1253acc4585716
Qian Li <liqian.cs@gmail.com>
2022-03-15 17:10:36 +0000

    add default scheduler as naive

--- scripts/start_worker.sh
+++ scripts/start_worker.sh
@@ -3,6 +3,18 @@ set -ex
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+SCHEDULER=""
+if [[ $# -eq 1 ]]; then
+    SCHEDULER="$1"
+elif [[ $# -gt 1 ]]; then
+    echo "Too many arguments!"
+    exit 1
+fi
+
+if [[ -z ${SCHEDULER} ]]; then
+    SCHEDULER="naive"
+fi
+
 cd ${SCRIPT_DIR}/../
 
 LOG_DIR="/var/tmp/apiary_worker"
@@ -15,7 +27,7 @@ mvn -DskipTests package
 LOGFILE=${LOG_DIR}"/worker.log"
 PIDFILE=${LOG_DIR}"/daemon.pid"
 
-java -verbose:gc -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xms4G -Xmx4G -jar target/apiary-worker-exec-fat-exec.jar -s $1 >${LOGFILE} 2>&1 &
+java -verbose:gc -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xms4G -Xmx4G -jar target/apiary-worker-exec-fat-exec.jar -s ${SCHEDULER} >${LOGFILE} 2>&1 &
 PID=$!
 echo $PID >>${PIDFILE}
 

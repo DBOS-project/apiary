9da0dc68ab6592bbea5554bc45038f0232d02c5c
Peter Kraft <petereliaskraft@gmail.com>
2022-03-22 10:28:31 -0700

    new scripts

--- scripts/initialize_multi_cockroachdb.sh
+++ scripts/initialize_multi_cockroachdb.sh
@@ -17,7 +17,23 @@ fi
 # Enter the root dir of the repo.
 cd ${SCRIPT_DIR}/../
 
-cockroach start --insecure --join=localhost:26257 --background
+DB_PORTS=(26257 26258 26259)
+JOIN_STR=""
+for port in ${DB_PORTS[@]}
+do
+    JOIN_STR+=,localhost:$port
+done
+JOIN_STR=${JOIN_STR:1}
+
+mkdir -p cockroach-data/
+for i in "${!DB_PORTS[@]}"; do
+    cockroach start \
+        --insecure --store=cockroach-data/node$i\
+        --listen-addr=localhost:${DB_PORTS[i]}\
+        --http-addr=localhost:$((8080 + $i))\
+        --join=$JOIN_STR\
+        --background
+done
 
 cockroach init --insecure --host=localhost:26257
 

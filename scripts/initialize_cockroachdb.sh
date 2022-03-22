9f8e9591679dc071a5610e77d07e88f1b38cf1a1
Peter Kraft <petereliaskraft@gmail.com>
2022-03-22 10:13:20 -0700

    Revert "experimental"
    This reverts commit 10f01ad693d6b33934f56ac1fc0df2e49856e865.

--- scripts/initialize_cockroachdb.sh
+++ scripts/initialize_cockroachdb.sh
@@ -20,14 +20,6 @@ if [[ -z $(command -v cockroach) ]]; then
     cockroach init --insecure --host=localhost
 fi
 
-HOSTNAME="localhost"
-if [[ $# -eq 1 ]]; then
-    HOSTNAME="$1"
-elif [[ $# -gt 1 ]]; then
-    echo "Too many arguments!"
-    exit 1
-fi
-
 # Enter the root dir of the repo.
 cd ${SCRIPT_DIR}/../
 
@@ -43,8 +35,8 @@ mkdir -p cockroach-data/
 for i in "${!DB_PORTS[@]}"; do
     cockroach start \
         --insecure --store=cockroach-data/node$i\
-        --listen-addr=HOSTNAME:${DB_PORTS[i]}\
-        --http-addr=HOSTNAME:$((8080 + $i))\
+        --listen-addr=localhost:${DB_PORTS[i]}\
+        --http-addr=localhost:$((8080 + $i))\
         --join=$JOIN_STR\
         --background
 done

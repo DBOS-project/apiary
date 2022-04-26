740b5bf89ea84899c4c4cb3c6a4e43e1d7cf3622
Qian Li <liqian.cs@gmail.com>
2022-04-25 21:14:19 -0700

    initial for postgres, minor fix stateless redundant code

--- src/main/java/org/dbos/apiary/interposition/StatelessFunction.java
+++ src/main/java/org/dbos/apiary/interposition/StatelessFunction.java
@@ -18,25 +18,4 @@ public class StatelessFunction implements ApiaryFunction {
     public ApiaryFunctionContext getContext() {
         return this.context;
     }
-
-    @Override
-    public FunctionOutput apiaryRunFunction(Object... input) {
-        // TODO: Logging.
-        // Use reflection to find internal runFunction.
-        Method functionMethod = Utilities.getFunctionMethod(this, "runFunction");
-        assert functionMethod != null;
-        Object output;
-        try {
-            output = functionMethod.invoke(this, input);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        if (output instanceof String) {
-            return context.getFunctionOutput((String) output);
-        } else {
-            assert (output instanceof ApiaryFuture);
-            return context.getFunctionOutput((ApiaryFuture) output);
-        }
-    }
 }

ad30bbe69bbe5ee0b54a6535851e374f941978d0
Peter Kraft <petereliaskraft@gmail.com>
2022-04-26 11:10:29 -0700

    nits

--- src/main/java/org/dbos/apiary/postgres/PostgresFunction.java
+++ src/main/java/org/dbos/apiary/postgres/PostgresFunction.java
@@ -1,8 +1,6 @@
 package org.dbos.apiary.postgres;
 
 import org.dbos.apiary.interposition.ApiaryFunction;
-import org.dbos.apiary.interposition.ApiaryFunctionContext;
-import org.dbos.apiary.interposition.ApiaryStatefulFunctionContext;
 
 public class PostgresFunction implements ApiaryFunction {
 }

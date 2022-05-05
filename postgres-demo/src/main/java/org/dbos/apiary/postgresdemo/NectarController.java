9a4c4dbe36b3a77dbb006fbe61b0e88cf471bd3d
Peter Kraft <petereliaskraft@gmail.com>
2022-05-05 12:13:04 -0700

    first demo function

--- postgres-demo/src/main/java/org/dbos/apiary/postgresdemo/NectarController.java
+++ postgres-demo/src/main/java/org/dbos/apiary/postgresdemo/NectarController.java
@@ -1,15 +1,21 @@
 package org.dbos.apiary.postgresdemo;
 
+import org.dbos.apiary.postgres.PostgresConnection;
+import org.dbos.apiary.postgresdemo.functions.NectarRegister;
+import org.dbos.apiary.utilities.ApiaryConfig;
+import org.dbos.apiary.worker.ApiaryNaiveScheduler;
+import org.dbos.apiary.worker.ApiaryWorker;
+import org.dbos.apiary.worker.ApiaryWorkerClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
-import org.voltdb.client.ProcCallException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -22,7 +28,18 @@ public class NectarController {
     AtomicInteger postID = new AtomicInteger(0);
     public static final int PKEY = 0;
 
-    public NectarController() {
+    ApiaryWorkerClient client;
+
+    public NectarController() throws SQLException {
+        PostgresConnection conn = new PostgresConnection("localhost", ApiaryConfig.postgresPort);
+        conn.dropTable("WebsiteLogins");
+        conn.createTable("WebsiteLogins", "(Username VARCHAR(1000) PRIMARY KEY NOT NULL, Password VARCHAR(1000) NOT NULL)");
+        conn.registerFunction("NectarRegister", NectarRegister::new);
+
+        ApiaryWorker apiaryWorker = new ApiaryWorker(conn, new ApiaryNaiveScheduler(), 4);
+        apiaryWorker.startServing();
+
+        this.client = new ApiaryWorkerClient();
     }
 
     @GetMapping("/")
@@ -42,7 +59,11 @@ public class NectarController {
     }
 
     @PostMapping("/registration")
-    public String registrationSubmit(@ModelAttribute Credentials credentials, Model model) throws InterruptedException, IOException, ProcCallException {
+    public String registrationSubmit(@ModelAttribute Credentials credentials, Model model) throws IOException {
+        int success = client.executeFunction("localhost", "NectarRegister", "nectarNetwork", credentials.getUsername(), credentials.getPassword()).getInt();
+        if (success != 0) {
+            return "redirect:/home";
+        }
         model.addAttribute("registration", credentials);
         return "registration_result";
     }
@@ -54,7 +75,7 @@ public class NectarController {
     }
 
     @PostMapping("/login")
-    public RedirectView loginSubmit(@ModelAttribute Credentials credentials, @ModelAttribute("logincredentials") Credentials logincredentials, RedirectAttributes attributes) throws InterruptedException, IOException, ProcCallException {
+    public RedirectView loginSubmit(@ModelAttribute Credentials credentials, @ModelAttribute("logincredentials") Credentials logincredentials, RedirectAttributes attributes) {
         boolean success = false;
         if (success) {
             logincredentials.setUsername(credentials.getUsername());
@@ -94,7 +115,7 @@ public class NectarController {
     }
 
     @PostMapping("/timeline")
-    public RedirectView timelinePostSubmit(@ModelAttribute WebPost webPost, @ModelAttribute("logincredentials") Credentials logincredentials, RedirectAttributes attributes) throws InterruptedException, IOException, ProcCallException {
+    public RedirectView timelinePostSubmit(@ModelAttribute WebPost webPost, @ModelAttribute("logincredentials") Credentials logincredentials, RedirectAttributes attributes) {
         if (logincredentials.getUsername() == null) {
             return new RedirectView("/home");
         }

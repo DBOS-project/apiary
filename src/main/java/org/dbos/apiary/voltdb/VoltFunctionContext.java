24315c5e1cd7347be298dad5b0e4789230ff4a45
Qian Li <liqian.cs@gmail.com>
2022-05-09 15:56:13 -0700

    debug

--- src/main/java/org/dbos/apiary/voltdb/VoltFunctionContext.java
+++ src/main/java/org/dbos/apiary/voltdb/VoltFunctionContext.java
@@ -57,9 +57,10 @@ public class VoltFunctionContext extends ApiaryStatefulFunctionContext {
 
     @Override
     public FunctionOutput checkPreviousExecution() {
-        p.voltQueueSQL(getRecordedOutput, this.execID, this.functionID);
+        p.voltQueueSQL(getRecordedOutput, this.execID, this.currentID);
         VoltTable v = p.voltExecuteSQL()[0];
         if (v.getRowCount() > 0) {
+            System.out.println(v.toFormattedString());
             v.advanceRow();
             List<Task> queuedTasks = List.of((Task[])Utilities.byteArrayToObject(v.getVarbinary(8)));
             Object o;

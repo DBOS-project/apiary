730d668fdd3be5e32cb10f6939316a474abbf6dd
Qian Li <liqian.cs@gmail.com>
2022-05-11 17:26:56 -0700

    update

--- src/main/java/org/dbos/apiary/postgres/PostgresConnection.java
+++ src/main/java/org/dbos/apiary/postgres/PostgresConnection.java
@@ -53,7 +53,6 @@ public class PostgresConnection implements ApiaryConnection {
             logger.info("Failed to connect to Postgres");
             throw new RuntimeException("Failed to connect to Postgres");
         }
-        dropTable("RecordedOutputs"); // TODO: Unique client IDs.
         createTable("RecordedOutputs", "ExecID bigint, FunctionID bigint, StringOutput VARCHAR(1000), IntOutput integer, StringArrayOutput bytea, IntArrayOutput bytea, FutureOutput bigint, QueuedTasks bytea, PRIMARY KEY(ExecID, FunctionID)");
         createTable("FuncInvocations", "APIARY_TRANSACTION_ID BIGINT NOT NULL, APIARY_TIMESTAMP BIGINT NOT NULL, EXECUTIONID BIGINT NOT NULL, SERVICE VARCHAR(1024) NOT NULL, PROCEDURENAME VARCHAR(1024) NOT NULL");
         createTable("ApiaryMetadata", "Key VARCHAR(1024) NOT NULL, Value Integer, PRIMARY KEY(key)");

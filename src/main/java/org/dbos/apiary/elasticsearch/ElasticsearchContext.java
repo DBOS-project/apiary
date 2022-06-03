ca14441f9d107cd4cc166d55af7b4f42fe205402
Peter Kraft <petereliaskraft@gmail.com>
2022-06-03 11:29:11 -0700

    stuff

--- src/main/java/org/dbos/apiary/elasticsearch/ElasticsearchContext.java
+++ src/main/java/org/dbos/apiary/elasticsearch/ElasticsearchContext.java
@@ -2,14 +2,20 @@ package org.dbos.apiary.elasticsearch;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.Refresh;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.json.JsonData;
 import org.dbos.apiary.function.ApiaryContext;
 import org.dbos.apiary.function.FunctionOutput;
 import org.dbos.apiary.function.WorkerContext;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ElasticsearchContext extends ApiaryContext {
     private final ElasticsearchClient client;
@@ -36,7 +42,7 @@ public class ElasticsearchContext extends ApiaryContext {
     public void executeUpdate(String index, ApiaryDocument document, String id) {
         try {
             document.setId(id);
-            document.setVersion(0);
+            document.setVersion(6);
             client.index(i -> i
                     .index(index)
                     .document(document)
@@ -46,8 +52,21 @@ public class ElasticsearchContext extends ApiaryContext {
         }
     }
 
-    public SearchResponse executeQuery(SearchRequest request, Class clazz) {
+    public SearchResponse executeQuery(String index, Query searchQuery, Class clazz) {
         try {
+            SearchRequest request = SearchRequest.of(s -> s
+                    .index(index).query(q -> q.bool(b -> b
+                                    .must(searchQuery)
+                                    .filter(List.of(
+                                            BoolQuery.of(bb -> bb
+                                                    .should(
+                                                            RangeQuery.of(f -> f.field("version").lte(JsonData.of(5)))._toQuery(),
+                                                            TermQuery.of(f -> f.field("version").value(6))._toQuery()
+                                                    )
+                                            )._toQuery())
+                            )
+                    ))
+            );
             return client.search(request, clazz);
         } catch (IOException e) {
             e.printStackTrace();

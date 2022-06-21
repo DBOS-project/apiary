6b06309a33651ce7f84692ee4d5f6a452915ff77
Peter Kraft <petereliaskraft@gmail.com>
2022-06-21 12:35:00 -0700

    fix

--- scripts/start_worker.sh
+++ scripts/start_worker.sh
@@ -38,7 +38,7 @@ mvn -T 1C -DskipTests package
 LOGFILE=${LOG_DIR}"/worker.log"
 PIDFILE=${LOG_DIR}"/daemon.pid"
 
-java -verbose:gc -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xms4G -Xmx4G -jar target/apiary-worker-exec-fat-exec.jar -s ${SCHEDULER} -db ${DBTYPE} -t ${NUMTHREADS} -secondaryAddr ${SECONDARYADDR} >${LOGFILE} 2>&1 &
+java -verbose:gc -XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -Xms4G -Xmx4G -jar target/apiary-worker-exec-fat-exec.jar -s ${SCHEDULER} -db ${DBTYPE} -t ${NUMTHREADS} -secondaryAddress ${SECONDARYADDR} >${LOGFILE} 2>&1 &
 PID=$!
 echo $PID >>${PIDFILE}
 

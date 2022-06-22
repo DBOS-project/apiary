046b7303cdd4cbb4cdd55b0be1463d0077b9983d
Qian Li <liqian.cs@gmail.com>
2022-06-22 21:35:56 +0000

    minor

--- scripts/initialize_mysql_docker.sh
+++ scripts/initialize_mysql_docker.sh
@@ -10,5 +10,8 @@ docker pull mysql
 # Set the password to dbos, default user is .
 docker run -d --network host --rm --name="apiary-mysql" --env MYSQL_ROOT_PASSWORD=dbos mysql:latest
 
+# Wait a bit.
+sleep 10
+
 # Create DBOS database.
 docker exec -i apiary-mysql mysql -hlocalhost -uroot -pdbos -t < init_mysql.sql

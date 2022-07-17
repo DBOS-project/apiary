7188ba5f4918e83a0b2c8798165362f417041444
Peter Kraft <petereliaskraft@gmail.com>
2022-07-17 08:53:20 -0700

    fixes

--- src/test/java/org/dbos/apiary/PostgresESTests.java
+++ src/test/java/org/dbos/apiary/PostgresESTests.java
@@ -9,9 +9,12 @@ import org.dbos.apiary.postgres.PostgresConnection;
 import org.dbos.apiary.procedures.elasticsearch.ElasticsearchBulkIndexPerson;
 import org.dbos.apiary.procedures.elasticsearch.ElasticsearchIndexPerson;
 import org.dbos.apiary.procedures.elasticsearch.ElasticsearchSearchPerson;
+import org.dbos.apiary.procedures.elasticsearch.shop.ShopESAddItem;
+import org.dbos.apiary.procedures.elasticsearch.shop.ShopESSearchItem;
 import org.dbos.apiary.procedures.postgres.pges.PostgresBulkIndexPerson;
 import org.dbos.apiary.procedures.postgres.pges.PostgresIndexPerson;
 import org.dbos.apiary.procedures.postgres.pges.PostgresSearchPerson;
+import org.dbos.apiary.procedures.postgres.shop.*;
 import org.dbos.apiary.utilities.ApiaryConfig;
 import org.dbos.apiary.worker.ApiaryNaiveScheduler;
 import org.dbos.apiary.worker.ApiaryWorker;
@@ -27,7 +30,8 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PostgresESTests {
     private static final Logger logger = LoggerFactory.getLogger(PostgresESTests.class);
@@ -40,7 +44,16 @@ public class PostgresESTests {
             PostgresConnection conn = new PostgresConnection("localhost", ApiaryConfig.postgresPort, "postgres", "postgres", "dbos");
             conn.dropTable("FuncInvocations");
             conn.dropTable("PersonTable");
+            conn.dropTable("ShopItems");
+            conn.dropTable("ShopCart");
+            conn.dropTable("ShopOrders");
+            conn.dropTable("ShopTransactions");
             conn.createTable("PersonTable", "Name varchar(1000) PRIMARY KEY NOT NULL, Number integer NOT NULL");
+            conn.createTable("ShopItems", "ItemID integer PRIMARY KEY NOT NULL, ItemName varchar(1000) NOT NULL, ItemDesc varchar(2000) NOT NULL, Cost integer NOT NULL, Inventory integer NOT NULL");
+            conn.createTable("ShopCart", "PersonID integer NOT NULL, ItemID integer NOT NULL, Cost integer NOT NULL");
+            conn.createTable("ShopOrders", "PersonID integer NOT NULL, OrderID integer NOT NULL, ItemID integer NOT NULL");
+            conn.createTable("ShopTransactions", "OrderID integer PRIMARY KEY NOT NULL, PersonID integer NOT NULL, Cost integer NOT NULL");
+            conn.createIndex("CREATE INDEX CartIndex ON ShopCart (PersonID);");
         } catch (Exception e) {
             logger.info("Failed to connect to Postgres.");
         }
@@ -58,7 +71,10 @@ public class PostgresESTests {
     public void cleanupElasticsearch() {
         try {
             ElasticsearchClient client = new ElasticsearchConnection("localhost", 9200, "elastic", "password").client;
-            DeleteIndexRequest request = new DeleteIndexRequest.Builder().index("people").build();
+            DeleteIndexRequest request;
+            request = new DeleteIndexRequest.Builder().index("people").ignoreUnavailable(true).build();
+            client.indices().delete(request);
+            request = new DeleteIndexRequest.Builder().index("items").ignoreUnavailable(true).build();
             client.indices().delete(request);
         } catch (Exception e) {
             logger.info("Index Not Deleted {}", e.getMessage());
@@ -296,5 +312,58 @@ public class PostgresESTests {
             t.join();
         }
         assertTrue(success.get());
+    }
+
+    @Test
+    public void testShopBenchmark() throws InvalidProtocolBufferException {
+        logger.info("testShopBenchmark");
+
+        ElasticsearchConnection conn;
+        PostgresConnection pconn;
+        try {
+            conn = new ElasticsearchConnection("localhost", 9200, "elastic", "password");
+            pconn = new PostgresConnection("localhost", ApiaryConfig.postgresPort, "postgres", "postgres", "dbos");
+        } catch (Exception e) {
+            logger.info("No Elasticsearch/Postgres instance! {}", e.getMessage());
+            return;
+        }
+
+        apiaryWorker = new ApiaryWorker(new ApiaryNaiveScheduler(), 4);
+        apiaryWorker.registerConnection(ApiaryConfig.elasticsearch, conn);
+        apiaryWorker.registerConnection(ApiaryConfig.postgres, pconn);
+        apiaryWorker.registerFunction("ShopAddItem", ApiaryConfig.postgres, ShopAddItem::new);
+        apiaryWorker.registerFunction("ShopSearchItem", ApiaryConfig.postgres, ShopSearchItem::new);
+        apiaryWorker.registerFunction("ShopAddCart", ApiaryConfig.postgres, ShopAddCart::new);
+        apiaryWorker.registerFunction("ShopCheckoutCart", ApiaryConfig.postgres, ShopCheckoutCart::new);
+        apiaryWorker.registerFunction("ShopGetItem", ApiaryConfig.postgres, ShopGetItem::new);
+        apiaryWorker.registerFunction("ShopESAddItem", ApiaryConfig.elasticsearch, ShopESAddItem::new);
+        apiaryWorker.registerFunction("ShopESSearchItem", ApiaryConfig.elasticsearch, ShopESSearchItem::new);
+        apiaryWorker.startServing();
+
+        ApiaryWorkerClient client = new ApiaryWorkerClient("localhost");
+        int res;
+        String[] items;
+        res = client.executeFunction("ShopAddItem", 0, "camera", "good camera", 5, 5).getInt();
+        res = client.executeFunction("ShopAddItem", 1, "stove", "good stove", 4, 1).getInt();
+        items = client.executeFunction("ShopSearchItem", "camera", 10).getStringArray();
+        assertEquals(1, items.length);
+        assertEquals("0", items[0]);
+        items = client.executeFunction("ShopSearchItem", "camera", 3).getStringArray();
+        assertEquals(0, items.length);
+        items = client.executeFunction("ShopSearchItem", "stove", 10).getStringArray();
+        assertEquals(1, items.length);
+        assertEquals("1", items[0]);
+        items = client.executeFunction("ShopSearchItem", "nothing", 10).getStringArray();
+        assertEquals(0, items.length);
+        res = client.executeFunction("ShopAddCart", 0, 0).getInt();
+        assertEquals(0, res);
+        res = client.executeFunction("ShopAddCart", 0, 1).getInt();
+        assertEquals(0, res);
+        res = client.executeFunction("ShopAddCart", 0, 1).getInt();
+        assertEquals(1, res);
+        res = client.executeFunction("ShopGetItem", 0, "camera", 10).getInt();
+        assertEquals(0, res);
+        res = client.executeFunction("ShopCheckoutCart", 0).getInt();
+        assertEquals(14, res);
     }
 }

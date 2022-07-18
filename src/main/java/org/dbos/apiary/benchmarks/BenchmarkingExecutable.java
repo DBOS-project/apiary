ac90627e796f76bfe63b79b7000f3d91c0ae8415
Peter Kraft <petereliaskraft@gmail.com>
2022-07-18 10:42:31 -0700

    defaults:

--- src/main/java/org/dbos/apiary/benchmarks/BenchmarkingExecutable.java
+++ src/main/java/org/dbos/apiary/benchmarks/BenchmarkingExecutable.java
@@ -79,13 +79,13 @@ public class BenchmarkingExecutable {
             MongoMicrobenchmark.benchmark(mainHostAddr, interval, duration, percentageRead, percentageNew, percentageUpdate);
         } else if (benchmark.equals("profile")) {
             int percentageRead = cmd.hasOption("p1") ? Integer.parseInt(cmd.getOptionValue("p1")) : 90;
-            int percentageNew = cmd.hasOption("p2") ? Integer.parseInt(cmd.getOptionValue("p2")) : 10;
-            int percentageUpdate = cmd.hasOption("p3") ? Integer.parseInt(cmd.getOptionValue("p3")) : 0;
+            int percentageNew = cmd.hasOption("p2") ? Integer.parseInt(cmd.getOptionValue("p2")) : 5;
+            int percentageUpdate = cmd.hasOption("p3") ? Integer.parseInt(cmd.getOptionValue("p3")) : 5;
             logger.info("Profile Benchmark {} {} {}", percentageRead, percentageNew, percentageUpdate);
             ProfileBenchmark.benchmark(interval, duration, percentageRead, percentageNew, percentageUpdate);
         } else if (benchmark.equals("gcsmicro")) {
-            int percentageRead = cmd.hasOption("p1") ? Integer.parseInt(cmd.getOptionValue("p1")) : 90;
-            int percentageNew = cmd.hasOption("p2") ? Integer.parseInt(cmd.getOptionValue("p2")) : 10;
+            int percentageRead = cmd.hasOption("p1") ? Integer.parseInt(cmd.getOptionValue("p1")) : 100;
+            int percentageNew = cmd.hasOption("p2") ? Integer.parseInt(cmd.getOptionValue("p2")) : 0;
             int percentageUpdate = cmd.hasOption("p3") ? Integer.parseInt(cmd.getOptionValue("p3")) : 0;
             logger.info("GCS Microbenchmark Benchmark {} {} {}", percentageRead, percentageNew, percentageUpdate);
             GCSMicrobenchmark.benchmark(interval, duration, percentageRead, percentageNew, percentageUpdate);

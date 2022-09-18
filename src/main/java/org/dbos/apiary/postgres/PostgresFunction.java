3d8fd71227803c10a0911efff3c4fb16a26c6174
Qian Li <liqian.cs@gmail.com>
2022-09-18 14:57:26 -0700

    debug

--- src/main/java/org/dbos/apiary/postgres/PostgresFunction.java
+++ src/main/java/org/dbos/apiary/postgres/PostgresFunction.java
@@ -4,11 +4,14 @@ import org.dbos.apiary.function.ApiaryContext;
 import org.dbos.apiary.function.ApiaryFunction;
 import org.dbos.apiary.utilities.ApiaryConfig;
 import org.dbos.apiary.utilities.Utilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * All Postgres functions should extend this class and implement <code>runFunction</code>.
  */
 public class PostgresFunction implements ApiaryFunction {
+    private static final Logger logger = LoggerFactory.getLogger(PostgresFunction.class);
 
     @Override
     public void recordInvocation(ApiaryContext ctxt, String funcName) {
@@ -18,6 +21,7 @@ public class PostgresFunction implements ApiaryFunction {
         }
         long timestamp = Utilities.getMicroTimestamp();
         long txid = ((PostgresContext) ctxt).txc.txID;
+        logger.info("Logging for funcName {}, time {}, txid {}", funcName, timestamp, txid);
         ctxt.workerContext.provBuff.addEntry(ApiaryConfig.tableFuncInvocations, txid, timestamp, ctxt.execID, ctxt.service, funcName);
     }
 }

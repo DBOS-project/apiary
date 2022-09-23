577a9e51d29bd8ad5985fcb0fd900f85376d457a
Qian Li <liqian.cs@gmail.com>
2022-09-22 18:24:57 -0700

    refactor tests

--- src/main/java/org/dbos/apiary/procedures/postgres/tests/PostgresProvenanceJoins.java
+++ src/main/java/org/dbos/apiary/procedures/postgres/tests/PostgresProvenanceJoins.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class PostgresProvenanceJoins extends PostgresFunction {
     private static final String addEntry = "INSERT INTO KVTable(KVKey, KVValue) VALUES (?, ?) ON CONFLICT (KVKey) DO NOTHING;";
     private static final String addEntryTwo = "INSERT INTO KVTableTwo(KVKeyTwo, KVValueTwo) VALUES (?, ?) ON CONFLICT (KVKeyTwo) DO NOTHING;";
-    private static final String getValue = "SELECT KVValue, KVValueTWO FROM KVTable, KVTableTwo WHERE KVKey = KVKeyTwo";
+    private static final String getValue = "SELECT KVValue, KVValueTWO FROM KVTable, KVTableTwo WHERE KVKey = KVKeyTwo;";
 
     public static int runFunction(PostgresContext ctxt, int key, int value, int valueTwo) throws SQLException {
         ctxt.executeUpdate(addEntry, key, value);

4c750f617cb3b678d829dae9655006dbdca263b8
Qian Li <liqian.cs@gmail.com>
2022-10-03 13:05:17 -0700

    debug

--- src/main/java/org/dbos/apiary/benchmarks/MysqlMicrobenchmark.java
+++ src/main/java/org/dbos/apiary/benchmarks/MysqlMicrobenchmark.java
@@ -50,7 +50,7 @@ public class MysqlMicrobenchmark {
         if (ApiaryConfig.XDBTransactions) {
             // TODO: need to solve the primary key issue. Currently cannot have primary keys.
             mysqlConn.createTable("PersonTable", "Name varchar(100) NOT NULL, Number integer NOT NULL, KEY (__apiaryID__), " +
-                    "  KEY(__endVersion__), " +  "  KEY(__beginVersion__)");
+                    "  KEY(__endVersion__), " +  "  KEY(__beginVersion__), KEY(Name)");
         } else {
             mysqlConn.createTable("PersonTable", "Name varchar(100) PRIMARY KEY NOT NULL, Number integer NOT NULL");
         }

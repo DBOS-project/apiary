ebf7651bfeb9bbd48ed10a5aafc7190bb82023b6
Qian Li <liqian.cs@gmail.com>
2022-10-25 22:09:41 +0000

    specify docker version

--- scripts/initialize_postgres_docker.sh
+++ scripts/initialize_postgres_docker.sh
@@ -4,7 +4,7 @@ set -ex
 SCRIPT_DIR=$(dirname $(realpath $0))
 
 # Start Postgres Docker image.
-docker pull postgres
+docker pull postgres:14.5-bullseye
 
 # Set the password to dbos, default user is postgres.
-docker run -d --network host --rm --name="apiary-postgres" --env POSTGRES_PASSWORD=dbos postgres:latest
+docker run -d --network host --rm --name="apiary-postgres" --env POSTGRES_PASSWORD=dbos postgres:14.5-bullseye

78163319bc044bdf4ac9ed6be867eb07ca3d82ae
Qian Li <liqian.cs@gmail.com>
2022-12-05 23:07:40 +0000

    track xact commit timestamp

--- scripts/initialize_postgres_docker.sh
+++ scripts/initialize_postgres_docker.sh
@@ -6,8 +6,20 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 # Start Postgres Docker image.
 docker pull postgres:14.5-bullseye
 
+PGCONFIG=""  # Postgres config file.
+if [[ $# -eq 1 ]]; then
+    PGCONFIG="$PWD/$1"
+fi
+
 # Set the password to dbos, default user is postgres.
-docker run -d --network host --rm --name="apiary-postgres" --env POSTGRES_PASSWORD=dbos postgres:14.5-bullseye
+if [[ -z "$PGCONFIG" ]]; then
+    docker run -d --network host --rm --name="apiary-postgres" --env POSTGRES_PASSWORD=dbos postgres:14.5-bullseye
+else
+    # Use customized config file
+    docker run -d --network host --rm --name="apiary-postgres" --env POSTGRES_PASSWORD=dbos \
+    -v "$PGCONFIG":/etc/postgresql/postgresql.conf \
+    postgres:14.5-bullseye -c 'config_file=/etc/postgresql/postgresql.conf'
+fi
 
 sleep 10
 

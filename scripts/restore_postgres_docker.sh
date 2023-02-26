402cf70262695761b4ce16f56e63fc74f0e1820b
Qian Li <liqian.cs@gmail.com>
2023-02-26 01:21:51 +0000

    update pg docker scripts, larger buffer pool

--- scripts/restore_postgres_docker.sh
+++ scripts/restore_postgres_docker.sh
@@ -7,28 +7,22 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 docker pull postgres:14.5-bullseye
 
 PGCONFIG=""  # Postgres config file.
-NAME="apiary-postgres"
+NAME="tpcc-postgres"
 PORT=5432
+
 if [[ $# -eq 1 ]]; then
     PGCONFIG="$PWD/$1"
 fi
 
-if [[ $# -eq 3 ]]; then
-    PGCONFIG="$PWD/$1"
-    NAME="$2"
-    PORT="$3"
-fi
 
 # Set the password to dbos, default user is postgres.
 if [[ -z "$PGCONFIG" ]]; then
-    docker run -d --network host --rm --name="$NAME" --env PGDATA=/var/lib/postgresql-static/data --env POSTGRES_PASSWORD=dbos postgres:14.5-bullseye
+    docker run -d --network host --rm --name="$NAME" --env PGDATA=/var/lib/postgresql-static/data --env POSTGRES_PASSWORD=dbos tpcc-postgres:latest
 else
     # Use customized config file
     docker run -d --network host --rm --name="$NAME" --env PGDATA=/var/lib/postgresql-static/data --env POSTGRES_PASSWORD=dbos \
     -v "$PGCONFIG":/etc/postgresql/postgresql.conf \
-    postgres:14.5-bullseye -c 'config_file=/etc/postgresql/postgresql.conf'
+    tpcc-postgres:latest -c 'config_file=/etc/postgresql/postgresql.conf'
 fi
 
-sleep 10
 
-docker exec -i $NAME psql -h localhost -U postgres -p $PORT -t < ${SCRIPT_DIR}/init_postgres.sql

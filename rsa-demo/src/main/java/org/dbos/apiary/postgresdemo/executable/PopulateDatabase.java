96ec84448a754896d8a6ac1384775ef46194f844
Peter Kraft <petereliaskraft@gmail.com>
2023-04-02 13:27:12 -0700

    populating works

--- rsa-demo/src/main/java/org/dbos/apiary/postgresdemo/executable/PopulateDatabase.java
+++ rsa-demo/src/main/java/org/dbos/apiary/postgresdemo/executable/PopulateDatabase.java
@@ -8,6 +8,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +17,10 @@ public class PopulateDatabase {
 
     private static final Logger logger = LoggerFactory.getLogger(PopulateDatabase.class);
 
-    public static void populateDatabase(Connection c, int numPeople) throws IOException {
+    private static final String register = "INSERT INTO WebsiteLogins(Username, Password) VALUES (?, ?);";
+    private static final String addPost = "INSERT INTO WebsitePosts(Sender, Receiver, PostText) VALUES (?, ?, ?);";
+
+    public static void populateDatabase(Connection c, int numPeople) throws IOException, SQLException {
 
         List<String> names = new ArrayList<>();
         List<String> posts = new ArrayList<>();
@@ -42,7 +47,27 @@ public class PopulateDatabase {
             names.add("name" + num++);
         }
 
-        logger.info("{}", names);
-        logger.info("{}", posts);
+        assert(posts.size() <= names.size());
+
+        PreparedStatement registerStatement = c.prepareStatement(register);
+        for (String name: names) {
+            registerStatement.setString(1, name);
+            registerStatement.setString(2, name);
+            registerStatement.addBatch();
+        }
+        registerStatement.executeBatch();
+        registerStatement.close();
+
+        PreparedStatement postStatement = c.prepareStatement(addPost);
+        for (String name: names) {
+            for (int postNum = 0; postNum < posts.size(); postNum++) {
+                postStatement.setString(1, names.get(postNum));
+                postStatement.setString(2, name);
+                postStatement.setString(3, posts.get(postNum));
+                postStatement.addBatch();
+            }
+        }
+        postStatement.executeBatch();
+        postStatement.close();
     }
 }

fcd7b8201df160dd0c49d53e6da7c942f5e2e256
Qian Li <liqian.cs@gmail.com>
2023-04-03 11:00:22 -0700

    setup vertica

--- rsa-demo/src/main/java/org/dbos/apiary/rsademo/NectarController.java
+++ rsa-demo/src/main/java/org/dbos/apiary/rsademo/NectarController.java
@@ -33,15 +33,15 @@ public class NectarController {
         ApiaryConfig.captureReads = true;
         ApiaryConfig.recordInput = true;
         ApiaryConfig.captureMetadata = true;
-        ApiaryConfig.provenancePort = 5432;  // Store provenance data in the same database.
+        ApiaryConfig.provenancePort = 5433;  // Store provenance data in Vertica.
 
-        PostgresConnection conn = new PostgresConnection("localhost", ApiaryConfig.postgresPort, "postgres", "dbos");
+        PostgresConnection conn = new PostgresConnection("localhost", ApiaryConfig.postgresPort, "postgres", "dbos", ApiaryConfig.vertica, "localhost");
         conn.dropTable("WebsiteLogins"); // For testing.
         conn.dropTable("WebsitePosts"); // For testing.
         conn.createTable("WebsiteLogins", "Username VARCHAR(1000) PRIMARY KEY NOT NULL, Password VARCHAR(1000) NOT NULL");
         conn.createTable("WebsitePosts", "Sender VARCHAR(1000) NOT NULL, Receiver VARCHAR(1000) NOT NULL, PostText VARCHAR(10000) NOT NULL");
 
-        ApiaryWorker apiaryWorker = new ApiaryWorker(new ApiaryNaiveScheduler(), 4, ApiaryConfig.postgres, ApiaryConfig.provenanceDefaultAddress);
+        ApiaryWorker apiaryWorker = new ApiaryWorker(new ApiaryNaiveScheduler(), 4, ApiaryConfig.vertica, "localhost");
         apiaryWorker.registerConnection(ApiaryConfig.postgres, conn);
         apiaryWorker.registerFunction("NectarRegister", ApiaryConfig.postgres, NectarRegister::new);
         apiaryWorker.registerFunction("NectarLogin", ApiaryConfig.postgres, NectarLogin::new);
